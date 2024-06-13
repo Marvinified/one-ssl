6e96a30429abecc55fa5142b9715a2ab27dcf238
Marvin Tunji-ola <marvinsmart9@gmail.com>
2024-06-13 14:49:13 +0100

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -9,3 +9,14 @@ snap install core
 echo "Removing Snap"
 yes | apt-get remove certbot
 
+echo "Install Certbot"
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+certbot certonly --standalone -n  -d $1
+
+
+
+
+
+
+
+
