ba5d77afc013e0ac1e649670357564fb7500087a
Marvin Tunji-ola <marvinsmart9@gmail.com>
2024-06-13 14:32:10 +0100

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,11 +1,11 @@
 # install snap
 echo "Installing Snap"
 apt update
-apt install snapd
+yes | apt install snapd
 snap install core
 
 # remove certbot
 echo "Removing Snap"
-yes | sudo apt-get remove certbot
+yes | apt-get remove certbot
 
 
